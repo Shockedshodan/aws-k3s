a0ace3a106b57e15aa9e0ecc5f3084b1fb746680
Shockedshodan <shockedshodan@gmail.com>
2025-08-20 18:09:18 +0200

    sg refactor

--- infra/main.tf
+++ infra/main.tf
@@ -93,107 +93,23 @@ module "ec2" {
   
 }
 
-resource "aws_security_group_rule" "allow_public_alb_to_httpbin" {
-  type                     = "ingress"
-  from_port                = 30080
-  to_port                  = 30080
-  protocol                 = "tcp"
-  security_group_id        = module.ec2.ec2_sg_id
-  source_security_group_id = module.lb.public_sg_id
-  description              = "Allow public ALB to reach app"
-}
+module "sg" {
+  source = "../modules/sg"
 
-resource "aws_security_group_rule" "allow_internal_alb_to_httpbin" {
-  type                     = "ingress"
-  from_port                = 30080
-  to_port                  = 30080
-  protocol                 = "tcp"
-  security_group_id        = module.ec2.ec2_sg_id
-  source_security_group_id = module.lb.internal_sg_id
-  description              = "Allow internal ALB to reach app"
-}
+  ec2_sg_id                  = module.ec2.ec2_sg_id
+  public_sg_id               = module.lb.public_sg_id
+  internal_sg_id             = module.lb.internal_sg_id
+  nlb_sg_id                  = module.lb.nlb_sg_id
+  vpc_endpoints_security_group_id = module.vpc.endpoints_security_group_id
 
-resource "aws_security_group_rule" "allow_nlb_to_ssh" {
-  type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  security_group_id        = module.ec2.ec2_sg_id
-  source_security_group_id = module.lb.nlb_sg_id
-  description              = "Allow NLB to reach ssh"
-}
+  ec2_instance_id            = module.ec2.instance_id
+  public_tg_arn              = module.lb.public_tg_arn
+  internal_tg_arn            = module.lb.internal_tg_arn
+  ssh_tg_arn                 = module.lb.ssh_tg_arn
+  k8s_tg_arn                 = module.lb.k8s_tg_arn
 
+  admin_cidrs                = ["77.165.233.0/24"]
 
-resource "aws_security_group_rule" "allow_ssh_from_admin" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  security_group_id = module.ec2.ec2_sg_id
-  cidr_blocks       = ["77.165.233.0/24"] 
-  description       = "SSH from admin CIDR via NLB"
-}
-
-resource "aws_security_group_rule" "allow_k8s_from_admin" {
-  type              = "ingress"
-  from_port         = 6443
-  to_port           = 6443
-  protocol          = "tcp"
-  security_group_id = module.ec2.ec2_sg_id
-  cidr_blocks       = ["77.165.233.0/24"]
-  description       = "Kubernetes API from admin CIDR via NLB"
-}
-
-resource "aws_security_group_rule" "allow_nlb_to_kubeapi" {
-  type                     = "ingress"
-  from_port                = 6443
-  to_port                  = 6443
-  protocol                 = "tcp"
-  security_group_id        = module.ec2.ec2_sg_id
-  source_security_group_id = module.lb.nlb_sg_id
-  description              = "Allow NLB to reach kubeapi"
-}
-resource "aws_security_group_rule" "vpc_endpoints_egress" {
-  type = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["10.0.101.0/24", "10.0.102.0/24"]
-  security_group_id = module.vpc.endpoints_security_group_id
-  description = "Allow outbound traffic to VPC endpoints"
-}
-resource "aws_security_group_rule" "ec2_to_vpc_endpoints" {
-  type = "egress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["10.0.0.0/16"]
-  security_group_id = module.ec2.ec2_sg_id
-  description = "Allow outbound traffic to VPC endpoints"
-}
-
-
-resource "aws_lb_target_group_attachment" "public" {
-  target_group_arn = module.lb.public_tg_arn
-  target_id        = module.ec2.instance_id
-  port             = 30080
-}
-resource "aws_lb_target_group_attachment" "internal" {
-  target_group_arn = module.lb.internal_tg_arn
-  target_id        = module.ec2.instance_id
-  port             = 30080
-}
-
-resource "aws_lb_target_group_attachment" "nlb_ssh" {
-  target_group_arn  = module.lb.ssh_tg_arn
-  target_id = module.ec2.instance_id
-  port = 22
-}
-
-resource "aws_lb_target_group_attachment" "nlb_k8s" {
-  target_group_arn  = module.lb.k8s_tg_arn
-  target_id = module.ec2.instance_id
-  port = 6443
 }
 
 
